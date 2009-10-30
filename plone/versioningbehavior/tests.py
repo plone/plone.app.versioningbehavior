20d38a1d24286a1b4f4344ab99272a16a1d92fda
Jonas Baumann <j.baumann@4teamwork.ch>
2009-10-30 13:32:32 +0000

    Load Products.CMFEditions before testing
    svn path=/plone.versioningbehavior/trunk/; revision=30906

--- plone/versioningbehavior/tests.py
+++ plone/versioningbehavior/tests.py
@@ -8,15 +8,18 @@ from Products.PloneTestCase.layer import onsetup
 
 import plone.app.dexterity
 import plone.versioningbehavior
+import Products.CMFEditions
 
 @onsetup
 def setup_product():
     zcml.load_config('meta.zcml', plone.app.dexterity)
     zcml.load_config('configure.zcml', plone.app.dexterity)
     zcml.load_config('configure.zcml', plone.versioningbehavior)
+    zcml.load_config('configure.zcml', Products.CMFEditions)
 
 setup_product()
-ptc.setupPloneSite(extension_profiles=['plone.app.dexterity:default',])
+ptc.setupPloneSite(extension_profiles=['plone.app.dexterity:default',
+                                       'Products.CMFEditions:CMFEditions'])
 
 doc_tests = (
 )
