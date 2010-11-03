cd30e39cba54c36f01c6ecfbb33b52c1a472c984
Jonas Baumann <j.baumann@4teamwork.ch>
2010-11-03 12:49:57 +0000

    setup.py: using version.txt
    svn path=/plone.versioningbehavior/trunk/; revision=45114

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = '0.1a1'
+version = open('plone/versioningbehavior/version.txt').read().strip()
 
 setup(name='plone.versioningbehavior',
       version=version,
