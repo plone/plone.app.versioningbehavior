2f61936fb60935b7b45f9a39e3ba2c49b54bf972
Jonas Baumann <j.baumann@4teamwork.ch>
2010-11-03 13:16:07 +0000

    cleanup
    svn path=/plone.versioningbehavior/trunk/; revision=45120

--- plone/versioningbehavior/utils.py
+++ plone/versioningbehavior/utils.py
@@ -1,17 +1,20 @@
-
 from zope.annotation.interfaces import IAnnotations
 from zope.publisher.interfaces.browser import IBrowserRequest
 
+
 def get_change_note(request, default=None):
-    """
-    Returns the changeNote submitted with this request. The changeNote
+    """Returns the changeNote submitted with this request. The changeNote
     is read from the form-field (see behaviors.IVersionable)
     """
+
     _marker = object()
+
     value = _marker
     if IBrowserRequest.providedBy(request):
         annotations = IAnnotations(request)
         value = annotations.get('plone.versioningbehavior-changeNote', _marker)
+
     if not value or value==_marker:
         return default
+
     return value
