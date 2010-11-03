9c91c30c36fec67b1807040ec7dedb5f09098838
Jonas Baumann <j.baumann@4teamwork.ch>
2010-11-03 12:58:01 +0000

    added z3c.autoinclude entry point ; clenaup
    svn path=/plone.versioningbehavior/trunk/; revision=45116

--- setup.py
+++ setup.py
@@ -5,17 +5,19 @@ version = open('plone/versioningbehavior/version.txt').read().strip()
 
 setup(name='plone.versioningbehavior',
       version=version,
-      description="Provides a behavior for using CMFEditions with dexterity content types",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      description='Provides a behavior for using CMFEditions with ' + \
+          'dexterity content types',
+      long_description=open("README.txt").read() + "\n" + \
+          open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='plone dexterity behavior versioning CMFEditions',
-      author='Jonas Baumann',
+      author='Jonas Baumann, 4teamwork GmbH',
       author_email='mailto:dexterity-development@googlegroups.com',
       url='http://plone.org/products/dexterity',
       license='GPL',
@@ -24,16 +26,18 @@ setup(name='plone.versioningbehavior',
       include_package_data=True,
       zip_safe=False,
       install_requires=[
-          'setuptools',
-          'Products.CMFEditions',
-          'five.grok',
-          'plone.dexterity',
-          'plone.directives.form',
-          'rwproperty',
-          'plone.autoform',
-          # -*- Extra requirements: -*-
-      ],
+        'setuptools',
+        'Products.CMFEditions',
+        'five.grok',
+        'plone.dexterity',
+        'plone.directives.form',
+        'rwproperty',
+        'plone.autoform',
+        # -*- Extra requirements: -*-
+        ],
       entry_points="""
       # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
