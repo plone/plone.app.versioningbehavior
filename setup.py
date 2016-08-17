aebf5ac3cf02cc48b063beff8c964af5a7db73c4
Maurits van Rees <maurits@vanrees.org>
2016-08-18 01:56:15 +0200

    Reindented setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -12,39 +12,40 @@ tests_require = [
     'plone.app.testing',
     'plone.namedfile[blobs]',
     'zope.app.intid',
-    ]
+]
 
-setup(name='plone.app.versioningbehavior',
-      version=version,
-      description='Provides a behavior for using CMFEditions with ' + \
-          'dexterity content types',
-      long_description=open("README.rst").read() + "\n" + \
-          open(os.path.join("CHANGES.rst")).read(),
-      # Get more strings from
-      # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
+setup(
+    name='plone.app.versioningbehavior',
+    version=version,
+    description=('Provides a behavior for using CMFEditions with '
+                 'dexterity content types'),
+    long_description=(open("README.rst").read() + "\n" +
+                      open(os.path.join("CHANGES.rst")).read()),
+    # Get more strings from
+    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
+        "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.0",
         "Framework :: Plone :: 5.1",
-        "Framework :: Plone :: 4.3",
         "Framework :: Zope2",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='plone dexterity behavior versioning CMFEditions',
-      author='Jonas Baumann, 4teamwork GmbH',
-      author_email='mailto:dexterity-development@googlegroups.com',
-      url='http://plone.org/products/dexterity',
-      license='GPL version 2',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone', 'plone.app'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+    ],
+    keywords='plone dexterity behavior versioning CMFEditions',
+    author='Jonas Baumann, 4teamwork GmbH',
+    author_email='mailto:dexterity-development@googlegroups.com',
+    url='http://plone.org/products/dexterity',
+    license='GPL version 2',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone', 'plone.app'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
         'plone.app.dexterity[relations]',
         'plone.autoform',
         'plone.dexterity',
@@ -53,12 +54,12 @@ setup(name='plone.app.versioningbehavior',
         'Products.CMFEditions>2.2.9',
         'setuptools',
         'zope.container',
-        ],
-      tests_require=tests_require,
-      extras_require=dict(tests=tests_require),
-      entry_points="""
-      # -*- Entry points: -*-
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    ],
+    tests_require=tests_require,
+    extras_require=dict(tests=tests_require),
+    entry_points="""
+    # -*- Entry points: -*-
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
