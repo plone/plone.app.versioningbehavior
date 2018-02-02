14c359fb09b5d50ad7a4751f04cc2a9921d6474c
Maurits van Rees <maurits@vanrees.org>
2018-02-02 17:12:28 +0100

    Removed Plone 4.3 classifier, added 5.2.
    [ci skip]

--- setup.py
+++ setup.py
@@ -30,9 +30,9 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Plone',
-        'Framework :: Plone :: 4.3',
         'Framework :: Plone :: 5.0',
         'Framework :: Plone :: 5.1',
+        'Framework :: Plone :: 5.2',
         'Framework :: Zope2',
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Operating System :: OS Independent',
